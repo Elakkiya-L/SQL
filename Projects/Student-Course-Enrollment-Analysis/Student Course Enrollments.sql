2e27bbde16a156c302bf0a8476b70c64049a2186
Elakkiya L <elakkiyaloganathan@outlook.com>
2026-04-24 23:59:01 +0530

    Update and rename queries.sql to Student Course Enrollments.sql

--- Projects/Student-Course-Enrollment-Analysis/Student Course Enrollments.sql
+++ Projects/Student-Course-Enrollment-Analysis/Student Course Enrollments.sql
@@ -44,3 +44,17 @@ left join Enrollments e on s.id=e.student_id
 group by s.name
 having count(e.course_id)>2;
 
+-- 7.Students enrolled in more than average number of courses
+-- Concepts: GROUP BY, HAVING, Subquery,join, AVG
+
+select s.name as Students_Name from Students s 
+join Enrollments e 
+on s.id=e.student_id 
+group by s.id,s.name
+having count(e.course_id)> (
+                            select avg(counts) 
+                            from
+                                (
+                                  Select count(student_id) as counts from enrollments 
+                                  group by student_id)t )
+;
