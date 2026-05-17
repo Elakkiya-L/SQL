84d9f4505726ca0c3e4f2e71d19575e34810f046
Elakkiya L <elakkiyaloganathan@outlook.com>
2026-05-18 03:06:06 +0530

    Create dates_where_traffic_dropped_by_30%.sql

---
+++ Projects/website-traffic/dates_where_traffic_dropped_by_30%.sql
@@ -0,0 +1,12 @@
+with daily_traffic as(
+select visit_date ,sum(visitors) as total_visitors 
+from website_traffic
+group by visit_date )
+,previous_day_traffic as (
+select visit_date,total_visitors, lag(total_visitors) over(order by visit_date) as prev_day_visitors
+ from daily_traffic)
+ select  visit_date,round ((prev_day_visitors-total_visitors)/prev_day_visitors*100 ,2)
+ from previous_day_traffic
+ where prev_day_visitors>0  and 
+ (prev_day_visitors-total_visitors) / prev_day_visitors >= 0.30
+ ;
